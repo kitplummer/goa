26c56c060ad533df8b1d984bc16d52c3c7f97560
Kit Plummer <kitplummer@gmail.com>
2021-12-18 10:03:34 -0500

    add: url validation and basic test

--- src/repos.rs
+++ src/repos.rs
@@ -1,4 +1,6 @@
 use std::io::Result;
+use std::io;
+use url::Url;
 
 #[derive(Debug)]
 pub struct Repo {
@@ -15,7 +17,28 @@ impl Repo {
 }
 
 pub fn spy_repo(url: String) -> Result<()> {
-    let repo = Repo::new(url);
-    println!("Spying repo: {:?}", repo);
-    Ok(())
+    let parsed_url = Url::parse(&url);
+    match parsed_url {
+        Ok(url) => {
+            let repo = Repo::new(String::from(url.as_str()));
+            println!("Spying repo: {:?}", repo);
+            Ok(())
+        },
+        _ => Err(io::Error::new(io::ErrorKind::InvalidData, "Invalid URL".to_string())),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_spy_repo_with_good_url() {
+        assert!(spy_repo(String::from("https://github.com/kitplummer/goa")).is_ok());
+    }
+
+    #[test]
+    fn test_spy_repo_with_bad_url() {
+        let res = spy_repo(String::from("test")).map_err(|e| e.kind());
+        assert_eq!(Err(io::ErrorKind::InvalidData), res);
+    }
 }
