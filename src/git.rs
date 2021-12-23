fa9ef8550f5dd4b887e679361caaf735b3e65f80
Kit Plummer <kitplummer@gmail.com>
2021-12-23 13:07:57 -0500

    test change

--- src/git.rs
+++ src/git.rs
@@ -109,65 +109,65 @@ fn print_stats(diff: &Diff) -> Result<(), git2::Error> {
     Ok(())
 }
 
-pub fn do_fetch<'a>(
-    repo: &'a git2::Repository,
-    refs: &[&str],
-    remote: &'a mut git2::Remote,
-) -> Result<git2::AnnotatedCommit<'a>, git2::Error> {
-    let mut cb = git2::RemoteCallbacks::new();
+// pub fn do_fetch<'a>(
+//     repo: &'a git2::Repository,
+//     refs: &[&str],
+//     remote: &'a mut git2::Remote,
+// ) -> Result<git2::AnnotatedCommit<'a>, git2::Error> {
+//     let mut cb = git2::RemoteCallbacks::new();
 
-    // Print out our transfer progress.
-    cb.transfer_progress(|stats| {
-        if stats.received_objects() == stats.total_objects() {
-            print!(
-                "Resolving deltas {}/{}\r",
-                stats.indexed_deltas(),
-                stats.total_deltas()
-            );
-        } else if stats.total_objects() > 0 {
-            print!(
-                "Received {}/{} objects ({}) in {} bytes\r",
-                stats.received_objects(),
-                stats.total_objects(),
-                stats.indexed_objects(),
-                stats.received_bytes()
-            );
-        }
-        io::stdout().flush().unwrap();
-        true
-    });
+//     // Print out our transfer progress.
+//     cb.transfer_progress(|stats| {
+//         if stats.received_objects() == stats.total_objects() {
+//             print!(
+//                 "Resolving deltas {}/{}\r",
+//                 stats.indexed_deltas(),
+//                 stats.total_deltas()
+//             );
+//         } else if stats.total_objects() > 0 {
+//             print!(
+//                 "Received {}/{} objects ({}) in {} bytes\r",
+//                 stats.received_objects(),
+//                 stats.total_objects(),
+//                 stats.indexed_objects(),
+//                 stats.received_bytes()
+//             );
+//         }
+//         io::stdout().flush().unwrap();
+//         true
+//     });
 
-    let mut fo = git2::FetchOptions::new();
-    fo.remote_callbacks(cb);
-    // Always fetch all tags.
-    // Perform a download and also update tips
-    fo.download_tags(git2::AutotagOption::All);
-    remote.fetch(refs, Some(&mut fo), None)?;
+//     let mut fo = git2::FetchOptions::new();
+//     fo.remote_callbacks(cb);
+//     // Always fetch all tags.
+//     // Perform a download and also update tips
+//     fo.download_tags(git2::AutotagOption::All);
+//     remote.fetch(refs, Some(&mut fo), None)?;
 
-    // If there are local objects (we got a thin pack), then tell the user
-    // how many objects we saved from having to cross the network.
-    let stats = remote.stats();
-    if stats.local_objects() > 0 {
-        println!(
-            "\rReceived {}/{} objects in {} bytes (used {} local \
-             objects)",
-            stats.indexed_objects(),
-            stats.total_objects(),
-            stats.received_bytes(),
-            stats.local_objects()
-        );
-    } else {
-        println!(
-            "\rReceived {}/{} objects in {} bytes",
-            stats.indexed_objects(),
-            stats.total_objects(),
-            stats.received_bytes()
-        );
-    }
+//     // If there are local objects (we got a thin pack), then tell the user
+//     // how many objects we saved from having to cross the network.
+//     let stats = remote.stats();
+//     if stats.local_objects() > 0 {
+//         println!(
+//             "\rReceived {}/{} objects in {} bytes (used {} local \
+//              objects)",
+//             stats.indexed_objects(),
+//             stats.total_objects(),
+//             stats.received_bytes(),
+//             stats.local_objects()
+//         );
+//     } else {
+//         println!(
+//             "\rReceived {}/{} objects in {} bytes",
+//             stats.indexed_objects(),
+//             stats.total_objects(),
+//             stats.received_bytes()
+//         );
+//     }
 
-    let fetch_head = repo.find_reference("FETCH_HEAD")?;
-    Ok(repo.reference_to_annotated_commit(&fetch_head)?)
-}
+//     let fetch_head = repo.find_reference("FETCH_HEAD")?;
+//     Ok(repo.reference_to_annotated_commit(&fetch_head)?)
+// }
 
 fn fast_forward(
     repo: &Repository,
