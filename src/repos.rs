579f951d812eaf2eefbf3550a836a870f214d8d3
Kit Plummer <kitplummer@gmail.com>
2022-01-14 12:46:27 -0500

    update: use run-script crate, init env output

--- src/repos.rs
+++ src/repos.rs
@@ -1,9 +1,13 @@
 use std::io::{Error, ErrorKind, Result};
 use std::ops::DerefMut;
+use std::path::PathBuf;
 use std::process::Command;
 use std::sync::{Arc, Mutex};
 use std::thread;
 use std::time::Duration;
+
+// For processing the command
+use run_script::ScriptOptions;
 
 // For datetime/timestamp/log
 use chrono::Utc;
@@ -187,7 +191,7 @@ pub fn do_process(repo: &mut Repo) -> Result<()> {
                     match do_task(repo) {
                         Ok(output) => {
                             let dt = Utc::now();
-                            println!("goa [{}]: stdout: {}", dt, output);
+                            println!("goa [{}]: command stdout: {}", dt, output);
                         }
                         Err(e) => {
                             let dt = Utc::now();
@@ -227,49 +231,71 @@ fn do_task(repo: &mut Repo) -> Result<String> {
             dt, command
         );
     }
+    let mut options = ScriptOptions::new();
+    options.working_directory = Some(PathBuf::from(&repo.local_path.as_ref().unwrap()));
 
-    let output = if cfg!(target_os = "windows") {
-        Command::new("cmd")
-        .current_dir(&repo.local_path.as_ref().unwrap())
-        .arg("/C")
-        .args(command)
-        .output()
-        .expect("goa error: failed to execute command on Windows.")
-    } else {
-        let mut command_command = "";
-        let mut command_args: Vec<&str> = [].to_vec();
+    let args = vec![];
 
-        for (pos, e) in command.iter().enumerate() {
-            if pos == 0 {
-                command_command = e;
-            } else {
-                command_args.push(e);
-            }
-        }
+    // run the script and get the script execution output
+    let (code, output, error) = run_script::run(
+        &repo.command,
+        &args,
+        &options,
+    )
+    .unwrap();
 
-        Command::new(command_command)
-        .current_dir(&repo.local_path.as_ref().unwrap())
-        .args(command_args)
-        .output()
-        .expect("goa error: failed to execute command")
-    };
+    // let output = if cfg!(target_os = "windows") {
+    //     Command::new("cmd")
+    //     .current_dir(&repo.local_path.as_ref().unwrap())
+    //     .arg("/C")
+    //     .args(command)
+    //     .output()
+    //     .expect("goa error: failed to execute command on Windows.")
+    // } else {
+    //     let mut command_command = "";
+    //     let mut command_args: Vec<&str> = [].to_vec();
+
+    //     for (pos, e) in command.iter().enumerate() {
+    //         if pos == 0 {
+    //             command_command = e;
+    //         } else {
+    //             command_args.push(e);
+    //         }
+    //     }
+
+    //     let args = command_args.join(" ").replace("\"", "");
+    //     println!("goa debug: command_args -> {args}");
+        // if args.is_empty() {
+        //     Command::new(command_command)
+        //     .current_dir(&repo.local_path.as_ref().unwrap())
+        //     .output()
+        //     .expect("goa error: failed to execute command")
+        // } else {
+        //     Command::new(command_command)
+        //     .current_dir(&repo.local_path.as_ref().unwrap())
+        //     .arg(args)
+        //     .output()
+        //     .expect("goa error: failed to execute command")
+        // }
+
+    //};
     let dt = Utc::now();
     if repo.verbosity > 2 {
         println!("goa debug: path -> {}", &repo.local_path.as_ref().unwrap());
-        println!("goa [{}]: command status: {}", dt, output.status);
+        println!("goa [{}]: command status: {}", dt, code);
     }
 
-    let stdout = String::from_utf8_lossy(&output.stdout);
+    //let stdout = String::from_utf8_lossy(&output);
 
     if repo.verbosity > 1 {
         println!(
             "goa [{}]: command stderr:\n{}",
             dt,
-            String::from_utf8_lossy(&output.stderr)
+            error
         );
     }
 
-    Ok(stdout.to_string())
+    Ok(output)
 }
 
 #[cfg(test)]
