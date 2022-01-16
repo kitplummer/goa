b44f4564331c18a89ff4f096866940f8bba2c758
Kit Plummer <kitplummer@gmail.com>
2022-01-16 15:43:37 -0500

    fix: exec_on_startup base commit

--- tests/cli.rs
+++ tests/cli.rs
@@ -48,9 +48,8 @@ fn test_spy_repo_command_bad_command() -> Result<(), Box<dyn std::error::Error>>
     cmd.arg("-e");
     cmd.arg("-c");
     cmd.arg("/notarealcommand");
-    cmd.assert().stderr(predicates::str::contains(
-        "/notarealcommand:",
-    ));
+    cmd.assert()
+        .stderr(predicates::str::contains("/notarealcommand:"));
     cmd.assert().failure().code(127);
     Ok(())
 }
@@ -63,9 +62,8 @@ fn test_spy_repo_command_bad_command_in_goa_file() -> Result<(), Box<dyn std::er
     cmd.arg("-e");
     cmd.arg("-v");
     cmd.arg("3");
-    cmd.assert().stderr(predicates::str::contains(
-        "/notarealcommand:",
-    ));
+    cmd.assert()
+        .stderr(predicates::str::contains("/notarealcommand:"));
     cmd.assert().failure().code(127);
     Ok(())
 }
